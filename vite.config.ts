8585c4c172998cae29dc11079826ad1caa0773bc
wangnan <wangnan_yx@163.com>
2024-06-17 10:47:33 +0800

    增加自动导入配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,22 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import postcssPresetEnv from 'postcss-preset-env'
+import postCssPxToRem from 'postcss-pxtorem'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    AutoImport({
+      imports: ['react', 'react-router-dom'],
+      dts: 'src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    })
   ],
   base: './',
   resolve: {
@@ -32,9 +43,27 @@ export default defineConfig({
           ],
           grid: true
         }),
+        postCssPxToRem({
+          rootValue: 16,
+          propList: ['*'],
+        }),
         postcssPresetEnv(),
         tailwindcss()
       ]
     }
+  },
+  server: {
+    port: 5173,
+    // host: true,
+    open: true,
+    // proxy: {
+    //   // 接口地址代理
+    //   '/api': {
+    //     target: 'http://data.aicnic.cn/', // 接口的域名
+    //     secure: false, // 如果是https接口，需要配置这个参数
+    //     changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+    //     rewrite: (path) => path.replace(/^\/api/, '/api')
+    //   }
+    // }
   }
 })
